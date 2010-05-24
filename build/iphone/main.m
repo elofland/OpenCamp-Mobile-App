ead71346abc0784a4edf0bfb07db063165c72373
elofland <eric@lofland.org>
2010-05-23 23:55:23 -0500

    the story thus far...

---
+++ build/iphone/main.m
@@ -0,0 +1,39 @@
+//
+//  Appcelerator Titanium Mobile
+//  WARNING: this is a generated file and should not be modified
+//
+
+#import <UIKit/UIKit.h>
+#define _QUOTEME(x) #x
+#define STRING(x) _QUOTEME(x)
+
+NSString * const TI_APPLICATION_DEPLOYTYPE = @"development";
+NSString * const TI_APPLICATION_ID = @"com.hypeanvil.opencamp";
+NSString * const TI_APPLICATION_PUBLISHER = @"elofland";
+NSString * const TI_APPLICATION_URL = @"http://www.hypeanvil.com";
+NSString * const TI_APPLICATION_NAME = @"OpenCamp";
+NSString * const TI_APPLICATION_VERSION = @"1.0";
+NSString * const TI_APPLICATION_DESCRIPTION = @"No description provided";
+NSString * const TI_APPLICATION_COPYRIGHT = @"2010 by Hype Anvil";
+NSString * const TI_APPLICATION_GUID = @"376d5fad-e7e1-44ae-a1d6-37dedc57c6eb";
+BOOL const TI_APPLICATION_ANALYTICS = true;
+
+#ifdef DEBUG
+NSString * const TI_APPLICATION_RESOURCE_DIR = @"/Users/elofland/Documents/hype_anvil/titanium/OpenCamp/build/iphone/build/Debug-iphonesimulator/OpenCamp.app";
+#endif
+
+int main(int argc, char *argv[]) {
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+
+#ifdef __LOG__ID__
+	NSArray *paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
+	NSString *documentsDirectory = [paths objectAtIndex:0];
+	NSString *logPath = [documentsDirectory stringByAppendingPathComponent:[NSString stringWithFormat:@"%s.log",STRING(__LOG__ID__)]];
+	freopen([logPath cStringUsingEncoding:NSUTF8StringEncoding],"w+",stderr);
+	fprintf(stderr,"[INFO] Application started\n");
+#endif
+
+    int retVal = UIApplicationMain(argc, argv, nil, nil);
+    [pool release];
+    return retVal;
+}
